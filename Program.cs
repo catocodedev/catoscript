ddcfa66dee3ba0e97ad5d99f8fd7098e08d386bf
EmeraldMike <EmeraldMike360@gmail.com>
2021-11-10 09:16:03 -0500

    meow readme = yes

--- Program.cs
+++ Program.cs
@@ -3,9 +3,22 @@ namespace cato
 {
     class cato
     {
-        static void Main(String[] args) 
+        static void Main(String[] args)
         {
-        Console.WriteLine("CatoScript Dev1.0.0");
+            string version = "Dev1.0.0";
+            Console.WriteLine("---------CatoScript Dev1.0.0----------");
+            Console.WriteLine("Waiting Input");
+            switch (Console.ReadLine())
+            {
+                case "ver":
+                    Console.WriteLine(version);
+                    break;
+                default:
+                    Console.WriteLine("Nothing. . .");
+                    break;
+            }
+            Console.Write("Press any key to close CatoScript...");
+            Console.ReadKey();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# CatoScript
+Now better than ever
