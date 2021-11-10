a54a5f215f00b95e330f67565a20655642151ba6
EmeraldMike <EmeraldMike360@gmail.com>
2021-11-10 08:30:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace cato
+{
+    class cato
+    {
+        static void Main(String[] args) 
+        {
+        Console.WriteLine("CatoScript Dev1.0.0");
+        }
+    }
+}
