032bcdeb6d486e0ebc4fe3ebe99d22b80eeca350
EmeraldMike <EmeraldMike360@gmail.com>
2022-01-25 11:59:30 -0500

    Added sys.run!

--- Program.cs
+++ Program.cs
@@ -586,6 +586,52 @@ namespace cato
                 case "if":
                     //to be made
                     break;
+                case "sys":
+                    if (!perams.EndsWith("#"))
+                    {
+                        try
+                        {
+                            parsed = perams.Split(new string[] { "\"" }, 3, StringSplitOptions.None)[1];
+                        }
+                        catch (Exception ex)
+                        {
+                            catoexception("PeramParseFail", "Console Parser could not parse |" + perams + "| to run!", op, opnum, 600);
+                        }
+                    }
+                    switch (subop)
+                    {
+                        case "run":
+                                try
+                            {
+
+                                System.Diagnostics.ProcessStartInfo procStartInfo =
+                                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + parsed);
+
+                                // The following commands are needed to redirect the standard output.
+                                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                                procStartInfo.RedirectStandardOutput = true;
+                                procStartInfo.UseShellExecute = false;
+                                // Do not create the black window.
+                                procStartInfo.CreateNoWindow = true;
+                                // Now we create a process, assign its ProcessStartInfo and start it
+                                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                                proc.StartInfo = procStartInfo;
+                                proc.Start();
+                                // Get the output into a string
+                                string result = proc.StandardOutput.ReadToEnd();
+                                // Display the command output.
+                                Console.WriteLine(result);
+                            }
+                            catch (Exception objException)
+                            {
+                                catoexception("SystemRunError", objException.ToString(), op, opnum, 700);
+                            }
+                            break;
+                        default:
+                            catoexception("Invaild SubOperation", subop + "Is not a vaild SubOperation of sys", op, opnum, 104);
+                            break;
+                    }
+                    break;
             default:
                     catoexception("InvalidOperation", "\"This Operation was not recognized.\"", op, opnum, 100);
             break;
