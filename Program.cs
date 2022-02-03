3bdd8da9c8f9c4dc26163c56d5957a1d1f0a7970
EmeraldMike <EmeraldMike360@gmail.com>
2022-02-03 09:49:10 -0500

    fixed run!

--- Program.cs
+++ Program.cs
@@ -851,18 +851,15 @@ namespace cato
                                     new System.Diagnostics.ProcessStartInfo("cmd","/c " + text);
                                 // The following commands are needed to redirect the standard output.
                                 // This means that it will be redirected to the Process.StandardOutput StreamReader.
-                                procStartInfo.RedirectStandardOutput = true;
-                                procStartInfo.UseShellExecute = false;
+                                procStartInfo.RedirectStandardOutput = false;
+                                procStartInfo.UseShellExecute = true;
                                 // Do not create the black window.
                                 procStartInfo.CreateNoWindow = true;
                                 // Now we create a process, assign its ProcessStartInfo and start it
                                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
                                 proc.StartInfo = procStartInfo;
                                 proc.Start();
-                                // Get the output into a string
-                                string result = proc.StandardOutput.ReadToEnd();
-                                // Display the command output.
-                                Console.WriteLine(result);
+                                proc.WaitForExit();
                             }
                             catch (Exception objException)
                             {
